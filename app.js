1ca5c96119b4e18d41e219843e42cde8188c4aef
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-02-07 11:21:53 +0100

    refactor: button style

--- app.js
+++ app.js
@@ -29,48 +29,48 @@ const Login = {
                     class: 'flex items-center justify-center flex-col p-4 gap-4 rounded-xl border bg-neutral-950 text-card-foreground shadow'
                 },
                     [
-                    h('legend', {
-                        class: 'text-xl font-semibold px-2'
-                    }, 'Login'),
-                    h('article', [
-                        h('label', {
-                            class: 'text-md font-medium',
-                            for: 'username'
-                        }, 'Username: '),
-                        h('input', {
-                            id: 'username',
-                            name: 'username',
-                            autocomplete: 'username',
-                            type: 'text',
-                            class: 'border border-input bg-transparent px-3 py-1 text-base shadow-sm rounded-md w-full',
-                            placeholder: "Entre ton Nom chacal",
-                            value: this.username,
-                            required: true,
-                            onInput: (e) => this.username = e.target.value
-                        })
-                    ]),
-                    h('article', [
-                        h('label', {
-                            class: 'text-md font-medium',
-                            for: 'password'
-                        }, 'Password: '),
-                        h('input', {
-                            id: 'password',
-                            name: 'password',
-                            autocomplete: 'current-password',
-                            class: 'border border-input bg-transparent px-3 py-1 text-base shadow-sm rounded-md w-full',
-                            type: 'password',
-                            placeholder: "Mot de passe",
-                            value: this.password,
-                            required: true,
-                            onInput: (e) => this.password = e.target.value
-                        })
-                    ]),
-                    h('button', {
-                        class: 'rounded-md text-sm font-medium bg-gray-50 text-gray-950 hover:bg-gray-50/70 h-9 px-4 py-2 w-full mt-4',
-                        type: 'submit',
-                    }, 'Login')
-                ])
+                        h('legend', {
+                            class: 'text-xl font-semibold px-2'
+                        }, 'Login'),
+                        h('article', [
+                            h('label', {
+                                class: 'text-md font-medium',
+                                for: 'username'
+                            }, 'Username: '),
+                            h('input', {
+                                id: 'username',
+                                name: 'username',
+                                autocomplete: 'username',
+                                type: 'text',
+                                class: 'border border-input bg-transparent px-3 py-1 text-base shadow-sm rounded-md w-full',
+                                placeholder: "Entre ton Nom chacal",
+                                value: this.username,
+                                required: true,
+                                onInput: (e) => this.username = e.target.value
+                            })
+                        ]),
+                        h('article', [
+                            h('label', {
+                                class: 'text-md font-medium',
+                                for: 'password'
+                            }, 'Password: '),
+                            h('input', {
+                                id: 'password',
+                                name: 'password',
+                                autocomplete: 'current-password',
+                                class: 'border border-input bg-transparent px-3 py-1 text-base shadow-sm rounded-md w-full',
+                                type: 'password',
+                                placeholder: "Mot de passe",
+                                value: this.password,
+                                required: true,
+                                onInput: (e) => this.password = e.target.value
+                            })
+                        ]),
+                        h('button', {
+                            class: 'rounded-md text-md font-medium bg-gray-50 text-gray-950 hover:bg-gray-50/70 py-2 w-full mt-4',
+                            type: 'submit',
+                        }, 'Login')
+                    ])
             ])
         );
     }
